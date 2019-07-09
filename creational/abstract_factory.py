c26fe8208c2d983093180e34f0d118ba06e8a6d9
whikwon <whikwon@gmail.com>
2019-07-10 08:53:54 +0900

    Add factory method, abstract factory

---
+++ creational/abstract_factory.py
@@ -0,0 +1,71 @@
+from abc import ABC, abstractmethod
+
+
+class PizzaStore(ABC):
+
+    @abstractmethod
+    def prepare_peperroni():
+        pass
+
+    @abstractmethod
+    def prepare_cheese():
+        pass
+
+
+class PizzaHut(PizzaStore):
+
+    def prepare_peperroni():
+        return BeefPepperoni()
+
+    def prepare_cheese():
+        return CheddarCheese()
+
+
+class DominoPizza(PizzaStore):
+
+    def prepare_peperroni():
+        return PorkPepperoni()
+
+    def prepare_cheese():
+        return GoudaCheese()
+
+
+class PizzaIngredient(ABC):
+    pass
+
+
+class Pepperoni(PizzaIngredient):
+    pass
+
+
+class BeefPepperoni(Pepperoni):
+    pass
+
+
+class PorkPepperoni(Pepperoni):
+    pass
+
+
+class Cheese(PizzaIngredient):
+    pass
+
+
+class CheddarCheese(Cheese):
+    pass
+
+
+class GoudaCheese(Cheese):
+    pass
+
+
+def main():
+    pizzahut = PizzaHut()
+    domino_pizza = DominoPizza()
+
+    for store in [pizzahut, domino_pizza]:
+        store.prepare_peperroni()
+        store.prepare_cheese()
+
+
+if __name__ == "__main__":
+    main()
---
+++ creational/factory_method.py
@@ -0,0 +1,45 @@
+from abc import abstractmethod, ABC
+
+
+class Stall(ABC):
+
+    @abstractmethod
+    def create_product():
+        pass
+
+
+class HotdogStall(Stall):
+
+    def prepare_food():
+        return Hotdog()
+
+
+class PizzaStall(Stall):
+
+    def prepare_food():
+        return Pizza()
+
+
+class Food(ABC):
+    pass
+
+
+class Hotdog(Food):
+    pass
+
+
+class Pizza(Food):
+    pass
+
+
+def main():
+    pizza_stall = PizzaStall()
+    hotdog_stall = HotdogStall()
+
+    food = []
+    for stall in [pizza_stall, hotdog_stall]:
+        food.append(stall.prepare_food)
+
+
+if __name__ == "__main__":
+    main()
