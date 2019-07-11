b72fca05824c1a5d65359558be87074228402730
whikwon <whikwon@gmail.com>
2019-07-11 09:15:52 +0900

    Add singleton

---
+++ creational/singleton.py
@@ -0,0 +1,18 @@
+class Singleton(object):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
+
+
+def main():
+    instanceA = Singleton()
+    instanceB = Singleton()
+    assert id(instanceA) == id(instanceB)
+    print(id(instanceA), id(instanceB))
+
+
+if __name__ == "__main__":
+    main()
