607ee164aa362b13b8f4ca9065c6cae52174b02b
Whi Kwon <30768596+whikwon@users.noreply.github.com>
2019-09-27 09:00:49 +0900

    Add prototype (#14)

---
+++ creational/prototype.py
@@ -0,0 +1,31 @@
+"""
+- https://www.reddit.com/r/Python/comments/657v4u/prototype_pattern_in_python/
+"""
+from copy import deepcopy
+
+
+class Prototype(object):
+    def clone(self, **attrs):
+        obj = deepcopy(self)
+        obj.__dict__.update(attrs)
+        return obj
+
+
+class Human(Prototype):
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+
+def main():
+    john = Human(name="John", weight=70)
+    cloned_john = john.clone(weight=50)
+
+    print(f"Original {john.name}'s weight: {john.weight}")
+    print(f"Original {john.name}'s id: {id(john)}")
+    print(f"Cloned {john.name}'s weight: {cloned_john.weight}")
+    print(f"Cloned {john.name}'s id: {id(cloned_john)}")
+
+
+if __name__ == "__main__":
+    main()
