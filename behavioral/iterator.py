5050db618946dc8985dcd5705402ec3b6fc0052b
Whi Kwon <30768596+whikwon@users.noreply.github.com>
2019-09-30 08:33:08 +0900

    Add iterator  (#11)

---
+++ behavioral/iterator.py
@@ -0,0 +1,43 @@
+"""
+https://www.slideshare.net/DamianGordon1/python-the-iterator-pattern
+"""
+
+
+class Collection(object):
+    def __init__(self):
+        self.data = []
+
+    def __iter__(self):
+        return Iterator(self.data)
+
+    def add_item(self, x):
+        self.data.append(x)
+
+
+class Iterator(object):
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+        self.num_data = len(self.data)
+
+    def __next__(self):
+        if self.index < self.num_data:
+            value = self.data[self.index]
+            self.index += 1
+            return value
+        else:
+            raise StopIteration()
+
+
+def main():
+    collection = Collection()
+    collection.add_item(1)
+    collection.add_item(2)
+    collection.add_item(3)
+
+    for item in collection:
+        print(item)
+
+
+if __name__ == "__main__":
+    main()
