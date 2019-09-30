ec6fe2904deaaf107fd74dd347e48603462ea2a6
whikwon <whikwon@gmail.com>
2019-10-01 08:20:44 +0900

    Add template

--- behavioral/template_method.py
+++ behavioral/template_method.py
@@ -9,11 +9,11 @@ from abc import ABC, abstractmethod
 class BaseDetector(ABC):
 
     @abstractmethod
-    def forward(self):
+    def forward(self, x):
         pass
 
     @abstractmethod
-    def extract_feat(self, img):
+    def extract_feat(self):
         pass
 
     @property
@@ -23,29 +23,48 @@ class BaseDetector(ABC):
 
 
 class OneStageDetector(BaseDetector):
-    def __init__(self, neck):
+    def __init__(self, neck=None):
         self.neck = neck
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        x = self.extract_feat(x)
+        if self.with_neck:
+            x = self.neck(x)
+        x += 1
+        return x
 
-    def extract_feat(self):
-        pass
+    def extract_feat(self, x):
+        return x // 2
 
 
 class TwoStageDetector(BaseDetector):
-    def __init__(self, neck):
+    def __init__(self, neck=None):
         self.neck = neck
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        x = self.extract_feat(x)
+        if self.with_neck:
+            x = self.neck(x)
+        x -= 1
+        return x
 
-    def extract_feat(self):
-        pass
+    def extract_feat(self, x):
+        return x // 4
+
+
+class Neck(object):
+    def __call__(self, x):
+        return x * 2
 
 
 def main():
-    pass
+    neck = Neck()
+    one_stage = OneStageDetector(neck)
+    two_stage = TwoStageDetector()
+
+    x = 10
+    assert one_stage.forward(x) == 11
+    assert two_stage.forward(x) == 1
 
 
 if __name__ == "__main__":
