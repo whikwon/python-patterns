4e732e431901c7f173fe8655b5fccf9130833ac1
whikwon <whikwon@gmail.com>
2019-10-04 08:21:19 +0900

    Modify factory method

--- creational/factory_method.py
+++ creational/factory_method.py
@@ -1,19 +1,25 @@
+"""
+- Provides an interface for creating objects in a superclass, but allows
+  subclasses to alter the type of objects that will be created.
+"""
 from abc import ABC, abstractmethod
 
 
 class Stall(ABC):
     @abstractmethod
-    def create_product():
+    def prepare_food(self):
         pass
 
 
 class HotdogStall(Stall):
-    def prepare_food():
+    def prepare_food(self):
+        print("Hotdog is ready.")
         return Hotdog()
 
 
 class PizzaStall(Stall):
-    def prepare_food():
+    def prepare_food(self):
+        print("Pizza is ready.")
         return Pizza()
 
 
@@ -35,7 +41,7 @@ def main():
 
     food = []
     for stall in [pizza_stall, hotdog_stall]:
-        food.append(stall.prepare_food)
+        food.append(stall.prepare_food())
 
 
 if __name__ == "__main__":
