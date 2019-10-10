48516891234a56f753b8e8eeb4bc1552416cdd0e
whikwon <whikwon@gmail.com>
2019-10-11 08:34:10 +0900

    Modify adapter pattern

--- structural/adapter.py
+++ structural/adapter.py
@@ -12,7 +12,7 @@ class EuropePowerPlug(PowerPlug):
 
     def plug_charger_in(self, item):
         assert self.voltage == item.voltage
-        print(f"{item.__class__.__name__} has plugged in")
+        print(f"{item.__class__.__name__} has plugged into {self.__class__.__name__}")
 
 
 class USPowerPlug(PowerPlug):
@@ -20,7 +20,7 @@ class USPowerPlug(PowerPlug):
 
     def plug_charger_in(self, item):
         assert self.voltage == item.voltage
-        print(f"{item.__class__.__name__} has plugged in")
+        print(f"{item.__class__.__name__} has plugged into {self.__class__.__name__}")
 
 
 class IPhoneCharger(ABC):
@@ -35,14 +35,45 @@ class EuropeIPhoneCharger(IPhoneCharger):
     voltage = 220
 
 
-class USEuropePowerPlugAdapter(EuropePowerPlug):
-    voltage = 110
+class PowerPlugAdapterFactory(ABC):
+    @abstractmethod
+    def get_powerplug(self):
+        pass
+
+    def set_voltage(self, item, voltage):
+        assert hasattr(item, "voltage")
+        item.voltage = voltage
+
+    def plug_charger_in(self, item):
+        self.set_voltage(item, self.adapter.voltage)
+        self.adapter.plug_charger_in(item)
+
+
+class USEuropePowerPlugAdapter(PowerPlugAdapterFactory):
+    def __init__(self):
+        self.adapter = self.get_powerplug()
+
+    def get_powerplug(self):
+        return EuropePowerPlug()
+
+
+class EuropeUSPowerPlugAdapter(PowerPlugAdapterFactory):
+    def __init__(self):
+        self.adapter = self.get_powerplug()
+
+    def get_powerplug(self):
+        return USPowerPlug()
 
 
 def main():
-    eur_iphone_charger = USIPhoneCharger()
-    power_plug = USEuropePowerPlugAdapter()
-    power_plug.plug_charger_in(eur_iphone_charger)
+    us_iphone_charger = USIPhoneCharger()
+    eur_iphone_charger = EuropeIPhoneCharger()
+
+    eur_us_adapter = EuropeUSPowerPlugAdapter()
+    eur_us_adapter.plug_charger_in(eur_iphone_charger)
+
+    us_eur_adapter = USEuropePowerPlugAdapter()
+    us_eur_adapter.plug_charger_in(us_iphone_charger)
 
 
 if __name__ == "__main__":
